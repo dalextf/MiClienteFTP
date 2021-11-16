8a4201cc29fd463fc180a659d4aeebcd7da4e3ec
fvarrui <4354486+fvarrui@users.noreply.github.com>
2021-11-16 16:16:32 +0000

    U salida en consola mejorada

--- src/main/java/dad/miclienteftp/Main.java
+++ src/main/java/dad/miclienteftp/Main.java
@@ -4,12 +4,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.SocketException;
+import java.util.Arrays;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
 public class Main {
-	
+
 	public static void main(String [] args) {
 		try {
 			
@@ -24,35 +25,26 @@ public class Main {
 			
 			// cambiar el directorio actual en el servidor
 			cliente.changeWorkingDirectory("/debian/dists");
-			
-			// recuperar un listado de los ficheros y directorios del directorio actual del servidor
-			FTPFile [] ficheros = cliente.listFiles();
-			
-			// recorrer el listado de archivos recuperados
-			System.out.println("Listar archivos del servidor:");
-			System.out.println("Nombre / Tamaño  / Tipo");
-			System.out.println("==================");
-			for (FTPFile fichero : ficheros) {
-				// mostrar el nombre, el tamaño (bytes) y el tipo (0 = fichero, 1 = directorio) de cada fichero/directorio recuperado
-				
-				String tipo = "";
-				switch (fichero.getType()) {
-				case FTPFile.DIRECTORY_TYPE: tipo = "Directorio"; break;
-				case FTPFile.FILE_TYPE: tipo = "Fichero"; break;
-				case FTPFile.SYMBOLIC_LINK_TYPE: tipo = "Enlace"; break;
-				default: tipo = "Desconocido"; break;
-				}
-				
-				System.out.println(fichero.getName() + " / " + fichero.getSize() + " bytes / " + tipo);
-				
-				
-			}
-			System.out.println();
-			
+
 			// recuperar el nombre del directorio actual
 			String directorioActual = cliente.printWorkingDirectory();
 			System.out.println("Directorio actual: " + directorioActual);
 
+			// recuperar un listado de los ficheros y directorios del directorio actual del servidor
+			FTPFile [] ficheros = cliente.listFiles();
+			
+			// recorrer el listado de archivos recuperados
+			System.out.format("+------------------------+%n");
+			System.out.format("| Archivos del servidor: |%n");
+			System.out.format("+------------------------+---------+----------------+-----------------+%n");
+			System.out.format("| Nombre                           | Tamaño (bytes) | Tipo            |%n");
+			System.out.format("+----------------------------------+----------------+-----------------+%n");
+			Arrays.stream(ficheros)
+				.forEach(fichero -> {
+				    System.out.format("| %-32s | %-14d | %-15s |%n", fichero.getName(), fichero.getSize(), getTipo(fichero));					
+				});
+			System.out.format("+----------------------------------+----------------+-----------------+%n");
+			
 			// cambiar el directorio padre en el servidor
 			cliente.changeWorkingDirectory("..");
 
@@ -72,6 +64,18 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public static String getTipo(FTPFile fichero) {
+		switch (fichero.getType()) {
+		case FTPFile.DIRECTORY_TYPE:
+			return "Directorio";
+		case FTPFile.FILE_TYPE:
+			return "Fichero";
+		case FTPFile.SYMBOLIC_LINK_TYPE:
+			return "Enlace";
+		default:
+			return "Desconocido";
+		}
+	}
 
 }
